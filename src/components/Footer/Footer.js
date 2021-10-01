90d8eeb1c8367c0d6aab5f8663f8994cf1a391ec
shryasss <shredya5101@gmail.com>
2021-10-01 16:24:33 +0530

    initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a href='/' className='link footer__link'>
+      Created By Shreyas Londhe
+    </a>
+  </footer>
+)
+
+export default Footer
---
+++ src/portfolio.js
@@ -0,0 +1,68 @@
+const header = {
+  // all the properties are optional - can be left empty or deleted
+  homepage: 'https://shryasss.github.io/shreyas-portfolio',
+  title: 'SL.',
+}
+
+const about = {
+  // all the properties are optional - can be left empty or deleted
+  name: 'Shreyas Londhe',
+  role: 'Web3 Developer',
+  description:
+    'My passion for software lies with dreaming up ideas and making them come true with elegant interfaces.',
+  resume: 'https://example.com',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/shreyas-londhe-999983192/',
+    github: 'https://github.com/shryasss',
+    twitter: 'https://twitter.com/shryasss',
+    instagram: 'https://www.instagram.com/shryasss/',
+  },
+}
+
+const projects = [
+  // projects can be added an removed
+  // if there are no projects, Projects section won't show up
+  {
+    name: 'My-Epic-NFT',
+    description: 'A Web3 app to mint your own unique NFTs',
+    stack: ['Solidity', 'JavaScript', 'React', 'Web3Js'],
+    sourceCode: 'https://github.com/my-epic-nft',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Crypto-Twitter',
+    description:
+      'A decentralized Twitter built using Solidity as backend and React as frontend.',
+    stack: ['Solidity', 'JavaScript', 'React', 'Web3Js'],
+    sourceCode: 'https://github.com/crypto-twitter-backend',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Wave-Portal',
+    description:
+      'My first Web3 app which resgisters waves from various users on the blockchain',
+    stack: ['Solidity', 'JavaScript', 'React', 'Web3Js'],
+    sourceCode: 'https://github.com/wave-portal',
+    livePreview: 'https://github.com',
+  },
+]
+
+const skills = [
+  // skills can be added or removed
+  // if there are no skills, Skills section won't show up
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'Solidity',
+  'Truffle',
+  'Web3Js',
+  'React',
+  'Git',
+]
+
+const contact = {
+  // email is optional - if left empty Contact section won't show up
+  email: 'shreyas_londhe@outlook.com',
+}
+
+export { header, about, projects, skills, contact }
